7b6d3d73c82d24f464332c8b040e9ffc0c5239eb
Asia Carter <asiamcarter@gmail.com>
2018-12-06 10:53:25 -0600

    emily's solutions to steve exercise

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -3,18 +3,36 @@ fetch("http://localhost:8088/food")
 .then(foods => foods.json())
 .then(parsedFoods => {
     parsedFoods.forEach(food => {
-        const foodAsHTML = foodFactory(food)
-        addFoodToDom(foodAsHTML)
-        console.log(foodFactory(food));
+        let foodAsHTML = foodFactory(food)
+        addFoodToDom(foodAsHTML);
+fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
+.then(response => response.json())
+.then(productInfo => { 
+
+let ingredients = productInfo.product.ingredients_text;
+let country = productInfo.product.countries_heirarchy;
+let calories = productInfo.product.nutriments.energy_100g
+let fat = productInfo.product.nutriments.fat_serving;
+let sugar = productInfo.product.nutriments.sugars_serving;
+
+
+
+
+
     })
 })
+})
+
+
 
 let foodFactory = (add) => {
     return `
     <div class = "foodPart">
     <h3>${add.name}</h3>
     <p>${add.type}</p>
-    <p>${add.ethnicity}</p>   
+    <p>${add.ethnicity}</p> 
+   
+    
     </div> 
     `
 }
@@ -25,6 +43,8 @@ let addFoodToDom = (foods) => {
     document.body.appendChild(container);
 
 }
+
+
 
 
 
