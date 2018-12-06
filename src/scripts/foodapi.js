80515c050b75dad20088bc4a076e03da3d4ebe39
Asia Carter <asiamcarter@gmail.com>
2018-12-06 13:44:36 -0600

    completed app

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -3,19 +3,18 @@ fetch("http://localhost:8088/food")
 .then(foods => foods.json())
 .then(parsedFoods => {
     parsedFoods.forEach(food => {
-        let foodAsHTML = foodFactory(food)
-        addFoodToDom(foodAsHTML);
+        
 fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
 .then(response => response.json())
 .then(productInfo => { 
 
-let ingredients = productInfo.product.ingredients_text;
-let country = productInfo.product.countries_heirarchy;
-let calories = productInfo.product.nutriments.energy_100g
-let fat = productInfo.product.nutriments.fat_serving;
-let sugar = productInfo.product.nutriments.sugars_serving;
-
-
+food.ingredients = productInfo.product.ingredients_text;
+food.country = productInfo.product.countries;
+food.calories = productInfo.product.nutriments.energy_serving;
+food.fat = productInfo.product.nutriments.fat_serving;
+food.sugar = productInfo.product.nutriments.sugars_serving;
+let foodAsHTML = foodFactory(food)
+addFoodToDom(foodAsHTML);
 
 
 
@@ -25,26 +24,30 @@ let sugar = productInfo.product.nutriments.sugars_serving;
 
 
 
-let foodFactory = (add) => {
-    return `
+let foodFactory = (foodObject) => {
+    let HTMLfoodSection = ( `
     <div class = "foodPart">
-    <h3>${add.name}</h3>
-    <p>${add.type}</p>
-    <p>${add.ethnicity}</p> 
+    <h3>${foodObject.name}</h3>
+    <p>${foodObject.type}</p>
+    <p>${foodObject.ethnicity}</p> 
+    <p>${foodObject.ingredients}</p>
+    <p>Sugars: ${foodObject.sugar}</p>
+    <p>Calories: ${foodObject.calories}</p>
+    <p>Fat: ${foodObject.fat}</p>
+ 
    
     
     </div> 
-    `
+    `)
+    return HTMLfoodSection;
 }
 
 let addFoodToDom = (foods) => {
     let container = document.querySelector(".foodList");
     container.innerHTML += foods;
-    document.body.appendChild(container);
+   
 
 }
-
-
 
 
 
